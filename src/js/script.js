9aa50e711972873a683524c9b33bd84611a24ca0
Max <maximpalaguta@gmail.com>
2021-04-04 04:31:41 +0300

    added and adapted sections <<supheader>>, <<header>>, <<promo>>, <<functions>>

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.promo__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrows/promo/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrows/promo/right.png"></button>',
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                dots: true,
+                arrows: false,
+            }
+        }, ]
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
+    })
+});
